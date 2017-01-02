db0331d912a063e68fa9688cf7819815fed228e6
Shameem A.R <aaduthoma19118@gmail.com>
2017-01-02 10:27:34 +0530

    Smooth Scrolling
    Implement smooth scrolling for links to sections

--- data/js/script.js
+++ data/js/script.js
@@ -182,6 +182,27 @@ $('.codenicely-bookmark-me').click(function(e) {
     return false;
 });
 
+$(function() {
+    
+    /*
+     * function     : smooth scroll when clicking id href links
+     * credits      : https://css-tricks.com/snippets/jquery/smooth-scrolling/
+     * */
+    
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 $.fn.extend(
 
     /*
