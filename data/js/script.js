20c2de7c1d497de42701437a04d974db0d75a1fa
Shameem A.R <aaduthoma19118@gmail.com>
2017-01-02 10:44:46 +0530

    Update script.js

--- data/js/script.js
+++ data/js/script.js
@@ -194,7 +194,7 @@ $(function() {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('body').animate({
+        $('html,body').animate({
           scrollTop: $(target).offset().top
         }, 1000);
         return false;
